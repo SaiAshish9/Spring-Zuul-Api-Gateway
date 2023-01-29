f6abe814456c3fb09d4f50ddd32cf10e309bfcec
SaiAshish <saiashish3760@gmail.com>
2023-01-29 23:13:55 +0530

    Add files via upload

---
+++ diagnosis-service/src/test/java/com/diagnosisservice/DiagnosisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diagnosisservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
